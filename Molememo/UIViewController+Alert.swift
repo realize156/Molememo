90bcb1e0946ab036fd12599a100e5526ff63a8cb
Park Jae Yun <wodbs720@gmail.com>
2021-01-05 20:47:10 +0900

    메모 저장

---
+++ Molememo/UIViewController+Alert.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController+Alert.swift
+//  Molememo
+//
+//  Created by 박재윤 on 2021/01/05.
+//
+
+import UIKit
+
+extension UIViewController{
+    func alert(title: String = "알림", message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+        
+        alert.addAction(okAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+}
